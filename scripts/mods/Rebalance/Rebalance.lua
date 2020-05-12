1984e189296ffcf9dca3ad39221337a89d5e8d71
RwAoNrDdOsM <benjicar@tpg.com.au>
2020-05-12 15:40:30 +0930

    Update Rebalance.lua

--- scripts/mods/Rebalance/Rebalance.lua
+++ scripts/mods/Rebalance/Rebalance.lua
@@ -16,3 +16,76 @@ for key, _ in pairs(NewDamageProfileTemplates) do
 end
 --Merge the tables together
 table.merge_recursive(DamageProfileTemplates, NewDamageProfileTemplates)
+
+--Do FS things
+for name, damage_profile in pairs(DamageProfileTemplates) do
+	if not damage_profile.targets then
+		damage_profile.targets = {}
+	end
+
+	fassert(damage_profile.default_target, "damage profile [\"%s\"] missing default_target", name)
+
+	if type(damage_profile.critical_strike) == "string" then
+		local template = PowerLevelTemplates[damage_profile.critical_strike]
+
+		fassert(template, "damage profile [\"%s\"] has no corresponding template defined in PowerLevelTemplates. Wanted template name is [\"%s\"] ", name, damage_profile.critical_strike)
+
+		damage_profile.critical_strike = template
+	end
+
+	if type(damage_profile.cleave_distribution) == "string" then
+		local template = PowerLevelTemplates[damage_profile.cleave_distribution]
+
+		fassert(template, "damage profile [\"%s\"] has no corresponding template defined in PowerLevelTemplates. Wanted template name is [\"%s\"] ", name, damage_profile.cleave_distribution)
+
+		damage_profile.cleave_distribution = template
+	end
+
+	if type(damage_profile.armor_modifier) == "string" then
+		local template = PowerLevelTemplates[damage_profile.armor_modifier]
+
+		fassert(template, "damage profile [\"%s\"] has no corresponding template defined in PowerLevelTemplates. Wanted template name is [\"%s\"] ", name, damage_profile.armor_modifier)
+
+		damage_profile.armor_modifier = template
+	end
+
+	if type(damage_profile.default_target) == "string" then
+		local template = PowerLevelTemplates[damage_profile.default_target]
+
+		fassert(template, "damage profile [\"%s\"] has no corresponding template defined in PowerLevelTemplates. Wanted template name is [\"%s\"] ", name, damage_profile.default_target)
+
+		damage_profile.default_target = template
+	end
+
+	if type(damage_profile.targets) == "string" then
+		local template = PowerLevelTemplates[damage_profile.targets]
+
+		fassert(template, "damage profile [\"%s\"] has no corresponding template defined in PowerLevelTemplates. Wanted template name is [\"%s\"] ", name, damage_profile.targets)
+
+		damage_profile.targets = template
+	end
+end
+
+local no_damage_templates = {}
+
+for name, damage_profile in pairs(DamageProfileTemplates) do
+	local no_damage_name = name .. "_no_damage"
+
+	if not DamageProfileTemplates[no_damage_name] then
+		local no_damage_template = table.clone(damage_profile)
+
+		if no_damage_template.targets then
+			for _, target in ipairs(no_damage_template.targets) do
+				if target.power_distribution then
+					target.power_distribution.attack = 0
+				end
+			end
+		end
+
+		if no_damage_template.default_target.power_distribution then
+			no_damage_template.default_target.power_distribution.attack = 0
+		end
+
+		no_damage_templates[no_damage_name] = no_damage_template
+	end
+end
